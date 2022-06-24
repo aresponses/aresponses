8b0fc85e1460334ca1add58e4a21c8b8b63ac2bb
Kevin C <Kache@users.noreply.github.com>
2022-06-23 17:33:48 -0700

    fix: incorrect pytest plugin entrypoint name
    Fixes: #71

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setup(
     python_requires=">=3.6",
     install_requires=["aiohttp==3.*,>=3.1.0", "pytest-asyncio"],
     # the following makes a plugin available to pytest
-    entry_points={"pytest11": ["name_of_plugin = aresponses.main"]},
+    entry_points={"pytest11": ["aresponses = aresponses.main"]},
 )
